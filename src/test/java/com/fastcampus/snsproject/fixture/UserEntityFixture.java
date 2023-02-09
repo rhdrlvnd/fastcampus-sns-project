50394e1dea4ef4ae4c4b16ebacca8788db2c1833
rhdrlvnd <rhdrlvnd@gmail.com>
2023-02-10 00:17:55 +0900

    commit

---
+++ src/test/java/com/fastcampus/snsproject/fixture/UserEntityFixture.java
@@ -0,0 +1,13 @@
+package com.fastcampus.snsproject.fixture;
+
+import com.fastcampus.snsproject.model.entity.UserEntity;
+
+public class UserEntityFixture {
+    public static UserEntity get(String userName, String password, Integer userId) {
+        UserEntity result = new UserEntity();
+        result.setId(userId);
+        result.setUserName(userName);
+        result.setPassword(password);
+        return result;
+    }
+}
